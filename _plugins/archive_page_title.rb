4aad451aee2bc962563adc2cd6fa7b90aaf86860
zzunstu <44315530@qq.com>
2017-09-04 10:48:56 +0800

    Create archive_page_title.rb

---
+++ _plugins/archive_page_title.rb
@@ -0,0 +1,32 @@
+module Jekyll
+	class ArchivePageTitleTag < Liquid:: Tag
+		ARCHIVE_PAGE_TYPES = %w(category tag year month day)
+		
+		def render(context)
+		unless Jekyll.const_defined?('Archives', false)
+			return ""
+		end
+		
+		page = context.environments.first['page']
+		
+		page_type = page['type']
+		unless ARCHIVE_PAGE_TYPES.include? page_type
+			return ""
+		end
+		
+		case page_type
+		when 'category'
+			"“#{page['title']}”分类的归档"
+		when 'tag'
+			"“#{page['title']}”分类的归档"
+		when 'year'
+			page['date'].strftime('%Y年') << '归档'
+		when 'month'
+			page['date'].strftime('%Y年-m月') << '归档'
+		when 'day'
+			page['date'].strftime('%Y年-m月-d日') << '归档'
+		end
+	end
+end
+
+Liquid:: Template.register_tag('archive_page_title', Jekyll::ArchivePageTitleTag)
