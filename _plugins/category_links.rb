009dae2c20a1da7bdb6765f6e5090eea2e03f83f
zzunstu <44315530@qq.com>
2017-09-04 09:58:58 +0800

    Update category_links.rb

--- _plugins/category_links.rb
+++ _plugins/category_links.rb
@@ -12,5 +12,14 @@ module Jekyll
       
       output.join(seperator)
     end
+    
+  def category_link(category, context)
+	  site = context.registers[:site]
+	  archive = Archives::Archive.new(site, category, 'category', [])
+	  url =  site.config['baseurl'] + archive.url
+	
+	  %(<a href="#{url}" title="#{category}">#{category}</a>)
+  end
+
    end
  end
